f91602677ebd7466fdb897d6fc2ebb854ccdb884
Erik Westrup <erik.westrup@gmail.com>
2022-03-22 13:58:52 +0000

    signup_path

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -14,6 +14,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get root_path
     assert_response :success
     assert_select "title", @base_title
+    assert_select "a[href=?]", signup_path
   end
 
   test "should get help" do
@@ -32,5 +33,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get contact_path
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
+  end
+
+  test "should get signup" do
+    get signup_path
+    assert_response :success
+    assert_select "title", "Sign up | #{@base_title}"
   end
 end
