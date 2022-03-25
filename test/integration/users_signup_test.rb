76ca8cb931ccb4a3e621d70a87d36e7a4963d7ff
Erik Westrup <erik.westrup@gmail.com>
2022-03-25 11:29:37 +0000

    Test error display

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,5 +10,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "foo" } }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
   end
 end
