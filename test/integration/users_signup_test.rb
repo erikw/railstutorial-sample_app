893be4d134527d805def00a67fa769bcc75e5356
Erik Westrup <erik.westrup@gmail.com>
2022-03-25 11:26:47 +0000

    Test faild signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,14 @@
+require "test_helper"
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do
+    gets signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: "",
+                                         email: "user@invalid",
+                                         password: "foo",
+                                         password_confirmation: "foo" } }
+    end
+    assert_template 'users/new'
+  end
+end
