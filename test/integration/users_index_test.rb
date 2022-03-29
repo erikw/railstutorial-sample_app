1a8182a475680d34b6534aa32f10851cdf276a4a
Erik Westrup <erik.westrup@gmail.com>
2022-03-29 18:29:02 +0000

    Assert two pagination bars

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -8,7 +8,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   test "index include pagination" do
     log_in_as(@user)
     get users_path
-    assert_template 'users/index'
+    assert_template 'users/index', count: 2
     assert_select 'div.pagination'
     User.paginate(page: 1).each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
