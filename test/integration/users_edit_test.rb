e15b3e13a37534eaddcc06615a6660716021ee42
Erik Westrup <erik.westrup@gmail.com>
2022-03-29 16:14:23 +0000

    Friendly forwarding URL on login

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -31,4 +31,21 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal name, @user.name
     assert_equal email, @user.email
   end
+
+  test "successful edit with friendy forwarding" do
+    get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
+    name = "New Name"
+    email = "new@mail.com"
+    patch user_path(@user), params: { user: { name: name,
+                                          email: email,
+                                          password: "",
+                                          password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
 end
