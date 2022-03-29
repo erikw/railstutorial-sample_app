6f7cdd807d122122132686b0ef20891e21ebb176
Erik Westrup <erik.westrup@gmail.com>
2022-03-29 15:59:44 +0000

    Require correct user on edit and update user actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -27,6 +27,11 @@ module SessionsHelper
     end
   end
 
+  def current_user?(user)
+    #user && user == current_user
+    user&. == current_user
+  end
+
   def logged_in?
     !current_user.nil?
   end
