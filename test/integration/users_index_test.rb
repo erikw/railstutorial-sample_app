0c10805a6ca4983e6700e4ed87ea38e0819f5656
Erik Westrup <erik.westrup@gmail.com>
2022-03-29 19:46:44 +0000

    Check correct count

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -2,15 +2,15 @@ require "test_helper"
 
 class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
-    @admin     = users(:michael)
+    @admin = users(:michael)
     @non_admin = users(:archer)
   end
 
   test "index as admin including pagination and delete links" do
     log_in_as(@admin)
     get users_path
-    assert_template 'users/index', count: 2
-    assert_select 'div.pagination'
+    assert_template 'users/index'
+    assert_select 'div.pagination', count: 2
     first_page_of_users = User.paginate(page: 1)
     first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
