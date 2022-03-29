e5a8e5b0f23ed3422fd173323b2fd640b0f297d9
Erik Westrup <erik.westrup@gmail.com>
2022-03-29 14:42:53 +0000

    Intergration test for invalid edit

---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,16 @@
+require "test_helper"
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+
+  test "Invalid edit shows error" do
+    get edit_user_path(@user)
+    patch user_path(@user), params: { user: { name: "",
+                                          email: "@invalid",
+                                          password: "something",
+                                          password_confirmation: "else" } }
+    assert_template 'users/edit'
+  end
+end
