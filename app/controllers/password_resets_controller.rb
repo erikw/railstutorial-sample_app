f4399c688352a9d952fc1ce48999a9c41440a27b
Erik Westrup <erik.westrup@gmail.com>
2022-04-04 13:48:59 +0000

    Password reset edit action

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,7 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user, only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+
   def new
   end
 
@@ -12,6 +15,22 @@ class PasswordResetsController < ApplicationController
     else
       flash.now[:danger] = "Email address not found"
       render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  private
+
+  def get_user
+    @user = User.find_by(email: params[:email].downcase)
+  end
+
+  def valid_user
+    unless @user && @user.activated? &&
+            @user.authenticated?(:reset, params[:id])
+            redirect_to root_url
     end
   end
 end
