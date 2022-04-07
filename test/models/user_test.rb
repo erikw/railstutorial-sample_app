d24b6930b7d03a9bf9ecfb0080f455deb7c05720
Erik Westrup <erik.westrup@gmail.com>
2022-04-07 11:25:25 +0000

    Test expected feed criteria

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -95,4 +95,22 @@ class UserTest < ActiveSupport::TestCase
     michael.follow(michael)
     assert_not michael.following?(michael)
   end
+
+  test "feed should have the right posts" do
+    michael = users(:michael)
+    archer = users(:archer)
+    lana = users(:lana)
+    # Posts from followed users
+    lana.micropost.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+    # Self-post included
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+    # Un-followed not included
+    lana.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
+    end
+  end
 end
